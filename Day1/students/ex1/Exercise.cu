38aeb80010509a299aaa23873ebd58b9f1eef1ca
linhtynny <linhduongvh.96@gmail.com>
2018-11-26 20:18:52 +0700

    Qs1

--- Day1/students/ex1/Exercise.cu
+++ Day1/students/ex1/Exercise.cu
@@ -1,10 +1,43 @@
 #include "Exercise.hpp"
+#include "chronoGPU.hpp"
+
+// class AdditionFunction : public thrust::binary_function
 
 void Exercise::Question1(const thrust::host_vector<int>& A,
 												const thrust::host_vector<int>& B, 
 												thrust::host_vector<int>&C) const
 {
 // TODO: addition of two vectors using thrust
+	ChronoGPU chrUP, chrDOWN chrGPU;
+
+	// thrust::device_vector<int> d_A(A);
+	// thrust::device_vector<int> d_C(A.size());
+
+	for (int i=3; --1; ){
+		chrUP.start();
+		thrust::device_vector<int> d_A(A);
+		thrust::device_vector<int> d_B(B);
+		thrust::device_vector<int> d_C(A.size());
+		chrUP.stop();
+
+		chrGPU.start();
+		thrust::transform(
+			d_A.begin(), d_A.end(), 
+			d_B.begin(),
+			d_C.begin(),
+			thrust::placeholders::_1+ thrust::placeholders::_2
+		);
+		chrGPU.stop();
+
+		chrDOWN.start();
+		C=d_C;
+		chrDOWN.stop();
+	}
+	float elapsed = chrUP.elapsedTime() + chrDOWN.elapsedTime() + chrGPU.elapsedTime();
+	std::cout << "Question1 done in " << elapsed << std::endl;
+	std::cout <<" - UP time : " << chrUP.elapsedTime() << std::endl;
+	std::cout <<" - GPU time : "<< chrGPU.elapsedTime() << std::endl;
+	std::cout <<" - DOWN time : " << chrDOWN.elapsedTime() << std::endl;
 }
 
 
