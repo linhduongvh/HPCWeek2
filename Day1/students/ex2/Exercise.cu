7fbea7e9d80eaaf002751efd347fee2698f71067
linhtynny <linhduongvh.96@gmail.com>
2018-11-27 19:04:06 +0700

    local

--- Day1/students/ex2/Exercise.cu
+++ Day1/students/ex2/Exercise.cu
@@ -1,13 +1,13 @@
 #include <thrust/scatter.h>
 #include <thrust/gather.h>
 #include "Exercise.hpp"
-#include "include/ChronoGPU.hpp"
+#include "include/chronoGPU.hpp"
 
 struct evenOddGather : public thrust::unary_function<const int, int>{
 	const int N;
 	evenOddGather(int size): N(size){}
 	__device__ int operator()(const int &i){
-		if (i*2)<N{
+		if ((i*2)<N){
 			return (i*2);
 		}else{
 			return (1 +i*2 -n);
@@ -21,7 +21,7 @@ void Exercise::Question1(const thrust::host_vector<int>& A,
 {
   // TODO: extract values at even and odd indices from A and put them into OE.
   // TODO: using GATHER
-	thrust::device_vector<int> gpuA = A;
+	thrust::device_vector<int> gpuA(A);
 	thrust::device_vector<int gpuOE(OE.size());
 
 	thrust::counting_iterator<int>X(0);
