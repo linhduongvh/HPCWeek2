3e0d53ca307eaf5ad3e066f64123f8f344b27f0e
linhtynny <linhduongvh.96@gmail.com>
2019-01-29 21:14:56 +0700

    add project

---
+++ Project/students/student3.cu
@@ -0,0 +1,13 @@
+#include "student3.hpp"
+
+/*
+* You have here to compute the segmented image from the filtered one.
+* Calculations have to be done on Device using Thrust.
+*
+* @param in: input (filtered) image
+* @param out: output (segmented) image
+* @param threshold: thresholding value (remove the edges greater than it)
+*/
+float student3(const PPMBitmap& in, PPMBitmap& out, const int threshold) {
+    return 0.f;
+}
---
+++ Project/students/student4.cu
@@ -0,0 +1,13 @@
+#include "student4.hpp"
+
+/*
+* You have here to compute the segmented image from the filtered one.
+* Calculations have to be done using Cuda.
+*
+* @param in: input (filtered) image
+* @param out: output (segmented) image
+* @param threshold: thresholding value (remove the edges greater than it)
+*/
+float student4(const PPMBitmap& in, PPMBitmap& out, const int threshold) {
+    return 0.f;
+}
