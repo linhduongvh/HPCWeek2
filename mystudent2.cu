6a3f12c103baa1789c4cbae4bb0b3b41da31400d
linhtynny <linhduongvh.96@gmail.com>
2019-01-30 20:51:34 +0700

    run ex 1 2

---
+++ mystudent2.cu
@@ -0,0 +1,15 @@
+#include "student2.hpp"
+
+/* Exercice 1.
+* Here, you have to apply the Median Filter on the input image.
+* Calculations have to be done using CUDA. 
+* You have to measure the computation times, and to return the number of ms 
+* your calculation took (do not include the memcpy).
+*
+* @param in: input image
+* @param out: output (filtered) image
+* @param size: width of the kernel 
+*/
+float student2(const PPMBitmap &in, PPMBitmap &out, const int size) {
+    return 0.f;
+}
